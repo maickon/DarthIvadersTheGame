ea5886a3928ed04444babce45731e75a038876cf
Maickon Rangel <maickonmaickon@hotmail.com>
2014-06-18 20:17:42 -0300

    Criando metodos para inicializar inimigos e checar colisoes na classeFase

--- src/game/Fase.java
+++ src/game/Fase.java
@@ -3,10 +3,12 @@ package game;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -18,6 +20,19 @@ public class Fase extends JPanel implements ActionListener{
 	private Image fundoImg;
 	private Nave nave;
 	private Timer tempo;
+	private boolean emJogo; 
+	private List<Inimigos> inimigos;
+	
+	private int[][] coordenadas = {
+			{2380,29},{2600,59},{1380,89},{780,109},
+			{580,139},{880,239},{790,259},{760,50},
+			{790,150},{1980,209},{560,45},{510,70},
+			{930,159},{590,80},{530,60},{940,59},
+			{990,30},{920,200},{900,259},{660,50},
+			{540,90},{810,220},{860,20},{740,180},
+			{820,128},{490,170},{700,30},{920,300},
+			{856,328},{456,320}
+	};
 
 	public Fase(){
 		setFocusable(true);
@@ -26,10 +41,20 @@ public class Fase extends JPanel implements ActionListener{
 		ImageIcon referenciaImg = new ImageIcon("src\\res\\fundo.png");
 		fundoImg = referenciaImg.getImage();
 		nave = new Nave();
+		
+		emJogo = true;
+		
 		tempo  = new Timer(5, this);
 		tempo.start();
 	}
 
+	public void inicializaInimigos(){
+		 inimigos = new ArrayList<Inimigos>();
+		 for(int i=0; i<= coordenadas.length; i++){
+			 inimigos.add(new Inimigos(coordenadas[i][0], coordenadas[0][i]));
+		 }
+	}
+	
 	public void paint(Graphics g){
 		
 		Graphics2D graficos = (Graphics2D) g;
@@ -60,6 +85,45 @@ public class Fase extends JPanel implements ActionListener{
 		
 		nave.mover();
 		repaint(); 
+	}
+	
+	public void checarColisoes(){
+		
+		Rectangle formaNave = nave.getBounds();
+		Rectangle formaInimigo;
+		Rectangle formaMissel;
+		
+		for(int i =0; i<inimigos.size(); i++){
+			
+			Inimigos tempInimigo = inimigos.get(i);
+			formaInimigo = tempInimigo.getBounds();
+			
+			if(formaNave.intersects(formaInimigo)){
+				nave.setVisivel(false);
+				tempInimigo.setVisivel(false);
+				emJogo = false;	 
+			}
+		}
+		
+		List<Missel> misseis = nave.getMisseis();
+		
+		for(int i=0; i<misseis.size(); i++){
+			
+			Missel tempMissel = misseis.get(i);
+			formaMissel = tempMissel.getBounds();
+			
+			for(int j=0; j<inimigos.size(); j++){
+				
+				Inimigos tempInimigo = inimigos.get(j);
+				formaInimigo = tempInimigo.getBounds(); 
+				
+				if(formaMissel.intersects(formaInimigo)){
+					
+					tempInimigo.setVisivel(false);
+					tempMissel.setVisivel(false);
+				}
+			}
+		}
 	}
 	
 	private class TecladoAdapter extends KeyAdapter{
